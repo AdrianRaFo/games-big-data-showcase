5a25c133e77b85d3275d97817ed38265f5cc8625
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2021-02-02 20:59:14 +0100

    format

--- modules/server/src/main/scala/adrianrafo/server/rawg/game/enums.scala
+++ modules/server/src/main/scala/adrianrafo/server/rawg/game/enums.scala
@@ -7,17 +7,12 @@ sealed trait EsrbRating extends Hyphencase
 
 object EsrbRating extends Enum[EsrbRating] with CirceEnum[EsrbRating] with DoobieEnum[EsrbRating] {
 
-  case object Everyone extends EsrbRating
-
+  case object Everyone       extends EsrbRating
   case object Everyone10Plus extends EsrbRating
-
-  case object Teen extends EsrbRating
-
-  case object Mature extends EsrbRating
-
-  case object AdultsOnly extends EsrbRating
-
-  case object RatingPending extends EsrbRating
+  case object Teen           extends EsrbRating
+  case object Mature         extends EsrbRating
+  case object AdultsOnly     extends EsrbRating
+  case object RatingPending  extends EsrbRating
 
   val values = findValues
 }
@@ -27,12 +22,9 @@ sealed trait RatingSlug extends EnumEntry with Lowercase
 object RatingSlug extends Enum[RatingSlug] with CirceEnum[RatingSlug] with DoobieEnum[RatingSlug] {
 
   case object Exceptional extends RatingSlug
-
   case object Recommended extends RatingSlug
-
-  case object Meh extends RatingSlug
-
-  case object Skip extends RatingSlug
+  case object Meh         extends RatingSlug
+  case object Skip        extends RatingSlug
 
   val values = findValues
 }
