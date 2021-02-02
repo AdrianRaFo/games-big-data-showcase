aa22b594f4053dabd2cfafdad035a0f3da7d6a2c
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2021-02-02 21:04:17 +0100

    create apply

--- modules/server/src/main/scala/adrianrafo/server/persistence/models.scala
+++ modules/server/src/main/scala/adrianrafo/server/persistence/models.scala
@@ -1,6 +1,6 @@
 package adrianrafo.server.persistence
 
-import adrianrafo.server.rawg.game.EsrbRating
+import adrianrafo.server.rawg.game.{EsrbRating, Game, RatingSlug}
 
 import java.time.{LocalDate, LocalDateTime}
 
@@ -33,3 +33,44 @@ final case class GameDB(
   skipRatedCount: Int,
   skipRatedPercent: Double
 )
+
+object GameDB {
+  def apply(game: Game): List[GameDB] = {
+
+    def getRatingCount(slug: RatingSlug): Int = game.ratings.find(_.title == slug).map(_.count).getOrElse(0)
+
+    def getRatingPercent(slug: RatingSlug): Double = game.ratings.find(_.title == slug).map(_.percent).getOrElse(0d)
+
+    game.platform.map(platform =>
+      new GameDB(
+        id = game.id,
+        slug = game.slug,
+        name = game.name,
+        released = game.released,
+        tba = game.tba,
+        rating = game.rating,
+        ratingTop = game.ratingTop,
+        ratingsCount = game.ratingsCount,
+        metacritic = game.metacritic,
+        playtime = game.playtime,
+        updated = game.updated,
+        reviewsCount = game.reviewsCount,
+        platformSlug = platform.slug,
+        platformReleasedAt = platform.releasedAt,
+        parentPlatforms = game.parentPlatforms.map(_.slug),
+        genres = game.genres.map(_.slug),
+        stores = game.stores.map(_.slug),
+        tags = game.tags.map(_.slug),
+        esrbRating = game.esrbRating,
+        exceptionalRatedCount = getRatingCount(RatingSlug.Exceptional),
+        exceptionalRatedPercent = getRatingPercent(RatingSlug.Exceptional),
+        recommendedRatedCount = getRatingCount(RatingSlug.Recommended),
+        recommendedRatedPercent = getRatingPercent(RatingSlug.Recommended),
+        mehRatedCount = getRatingCount(RatingSlug.Meh),
+        mehRatedPercent = getRatingPercent(RatingSlug.Meh),
+        skipRatedCount = getRatingCount(RatingSlug.Skip),
+        skipRatedPercent = getRatingPercent(RatingSlug.Skip)
+      )
+    )
+  }
+}
