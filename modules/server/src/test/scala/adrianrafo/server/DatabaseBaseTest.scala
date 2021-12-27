d0ddc16880001410f6fece1feeabbc33e90325c6
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2021-12-27 19:28:10 +0100

    fix tests

--- modules/server/src/test/scala/adrianrafo/server/DatabaseBaseTest.scala
+++ modules/server/src/test/scala/adrianrafo/server/DatabaseBaseTest.scala
@@ -7,12 +7,15 @@ import com.dimafeng.testcontainers.PostgreSQLContainer
 import com.dimafeng.testcontainers.munit.TestContainerForAll
 import doobie.Transactor
 import munit.FunSuite
+import org.testcontainers.utility.DockerImageName
 
 trait DatabaseBaseTest extends FunSuite with TestContainerForAll {
 
   private val driverName: String = "org.postgresql.Driver"
 
-  override val containerDef: PostgreSQLContainer.Def = PostgreSQLContainer.Def()
+  override val containerDef: PostgreSQLContainer.Def = PostgreSQLContainer.Def(
+    dockerImageName = DockerImageName.parse("postgres:14.1")
+  )
 
   // noinspection TypeAnnotation
   lazy val transactor = withContainers { container =>
